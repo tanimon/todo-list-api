d1d88581ce68165e9c4b43272f5a8f7dd19d53a4
tanimon <8575113+tanimon@users.noreply.github.com>
2021-10-28 21:09:35 +0900

    Add code to setup Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 
 let cachedServer: Handler;
 
-async function bootstrap(): Promise<Handler> {
+function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('ToDo list API')
+    .setDescription(
+      'This is an API documentation of a simple ToDo list API server.',
+    )
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
+async function bootstrapHandler(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
@@ -18,6 +34,13 @@ export const handler: Handler = async (
   context: Context,
   callback: Callback,
 ) => {
-  cachedServer = cachedServer ?? (await bootstrap());
+  if (event.path === '/api') {
+    event.path = '/api/';
+  }
+  event.path = event.path.includes('swagger-ui')
+    ? `/api${event.path}`
+    : event.path;
+
+  cachedServer = cachedServer ?? (await bootstrapHandler());
   return cachedServer(event, context, callback);
 };
