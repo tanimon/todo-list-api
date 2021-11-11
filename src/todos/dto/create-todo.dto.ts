40bad80a82c0152f119b267d160d155e8ce54947
tanimon <8575113+tanimon@users.noreply.github.com>
2021-11-11 20:58:42 +0900

    Implement DTOs

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,1 +1,17 @@
-export class CreateTodoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTodoDto {
+  @ApiProperty({
+    example: '牛乳を買う',
+    description: 'Todo アイテムのタイトル。',
+  })
+  readonly title: string;
+
+  @ApiProperty({
+    example: 'スーパー A で特売日に牛乳を買う。',
+    description: 'Todo アイテムの内容。',
+  })
+  readonly description: string;
+
+  userId: string;
+}
--- src/todos/dto/update-todo.dto.ts
+++ src/todos/dto/update-todo.dto.ts
@@ -1,4 +1,24 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTodoDto } from './create-todo.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto {
+  @ApiProperty({
+    example: '牛乳を買う',
+    description: 'Todo アイテムのタイトル。',
+    required: false,
+  })
+  readonly title?: string;
+
+  @ApiProperty({
+    example: 'スーパー A で特売日に牛乳を買う。',
+    description: 'Todo アイテムの内容。',
+    required: false,
+  })
+  readonly description?: string;
+
+  @ApiProperty({
+    example: false,
+    description: 'Todo アイテムが完了したかどうか。',
+    required: false,
+  })
+  readonly completed?: boolean;
+}
